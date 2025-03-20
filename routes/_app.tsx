5e485cd3192fe863ff04f3cf3ae00a854bdf4f3d
Joy <serotoninoy@gmail.com>
2025-03-21 06:47:29 +0700

    favicon and title change

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,7 +7,7 @@ export default define.page(({ Component }) => {
 				<meta charset="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="google" content="notranslate" />
-				<title>Ujian-v2</title>
+				<title>Ujian</title>
 				<link rel="stylesheet" href="/styles.css" />
 			</head>
 			<body class="h-dvh">
